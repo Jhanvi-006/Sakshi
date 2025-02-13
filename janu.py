234678f4174664fc46f8866ed6901bf491294241
Jhanvi <jhanviybhavsar.bscibm2024@iar.ac.in>
2025-02-13 13:22:17 +0530

    Python File

---
+++ janu.py
@@ -0,0 +1,10 @@
+A = 12
+B = 50
+C= 6
+ if A >= B and A >= C:
+        print(f" A is greater number")
+    elif B >= A and B >= C:
+        print(f"B is greater number")
+    else:
+        print(f" C is greater number")
+        
